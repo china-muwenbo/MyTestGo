a0d00f24e8ddebf909396c6135d9b3dc79f8d809
dylan one <18729243053@163.com>
2018-03-01 19:05:45 +0800

    3月1

--- src/com.dylan.main/TestInterface.go
+++ src/com.dylan.main/TestInterface.go
@@ -1,9 +1,51 @@
 package main
+/*
+
+注意：Go语言支持的除Interface类型外的任何其它数据类型都可以定义其method（而并非只有struct才支持method），
+		只不过实际项目中，method(s)多定义在struct上而已。
+
+1.接口是类型 type
+2.是一堆方法的集合
+//总结 接口是拥有一堆方法的集合 类型
+*/
+// 如果
+
 
 func main() {
+p:=new(person)
+p.sporter();
+
+p.sporter()
 
 }
 
-type test1 interface {
-	String1() string
+// 结构体
+type person struct{
+
+}
+//跑的接口
+type runType interface{
+	firstrun()
+
+}
+//跑的接口
+type smileType interface{
+	firstsmlie()
+}
+
+
+//
+func  (* person) firstrun(){
+
+}
+func  (* person) firstsmlie(){
+
+}
+
+
+func  (* person) thief(){
+
+}
+func (*person) sporter()  {
+
 }
---
+++ src/com.dylan.main/email/func.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"gopkg.in/gomail.v2"
+	"fmt"
+)
+
+func main() {
+	m := gomail.NewMessage()
+
+	m.SetHeader("From", "18729243053@163.com")
+
+	m.SetHeader("To", "1061792506@qq.com")
+
+	m.SetAddressHeader("Cc", "1679496833@qq.com", "Dan")
+	//使用Subject 指明
+	m.SetHeader("Subject", "Hello!")
+
+	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
+	// 居然给老子标记垃圾邮件
+	d := gomail.NewDialer("smtp.163.com", 465, "18729243053@163.com", "muwenbo100")
+
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}else {
+		fmt.Println("发送完成")
+
+	}
+}
