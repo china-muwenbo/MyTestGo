caa4dd04a38232f7ffdc6e3deb66a966da32c025
git <1061792506@qq.com>
2018-03-29 23:58:47 +0800

    添加对list的什么研究

--- src/com.dylan.main/list/list.go
+++ src/com.dylan.main/list/list.go
@@ -23,49 +23,50 @@ func main() {
 	}
 	//获取l 最前的元素
 	et1:=l.Front();
-	fmt.Println("list 最前的元素 ",et1.Value)
+	fmt.Println("list 最前的元素 Front  ",et1.Value)
 	//获取l 最后的元素
 	et2:=l.Back()
-	fmt.Println("list 最后的元素 ",et2.Value)
+	fmt.Println("list 最后的元素  Back ",et2.Value)
 	//获取l的长度
-	fmt.Println("list 的长度为：",l.Len())
+	fmt.Println("list 的长度为： Len ",l.Len())
 	//向后移动
 	l.MoveAfter(e1,e2)
-	fmt.Println("把1元素移动到2元素的后面 向后移动后:")
+	fmt.Println("把1元素移动到2元素的后面 向后移动后 MoveAfter :")
 	for e := l.Front(); e != nil; e = e.Next() {
 		fmt.Print(e.Value," ")
 	}
 	//向前移动
 	l.MoveBefore(e1,e2)
-	fmt.Println("\n把1元素移动到2元素的前面 向前移动后:")
+	fmt.Println("\n把1元素移动到2元素的前面 向前移动后 MoveBefore :")
 	for e := l.Front(); e != nil; e = e.Next() {
 		fmt.Print(e.Value," ")
 	}
 	//移动到最后面
 	l. MoveToBack(e1)
-	fmt.Println("\n 1元素出现在最后面")
+	fmt.Println("\n 1元素出现在最后面 MoveToBack ")
 	for e := l.Front(); e != nil; e = e.Next() {
 		fmt.Print(e.Value," ")
 	}
 	//移动到最前面
 	l. MoveToFront(e1)
-	fmt.Println("\n 1元素出现在最前面")
+	fmt.Println("\n 1元素出现在最前面 MoveToFront ")
 	for e := l.Front(); e != nil; e = e.Next() {
 		fmt.Print(e.Value," ")
 	}
 	//删除元素
 	fmt.Println("")
 	l.Remove(e1)
-	fmt.Println("\n e1元素移除后")
+	fmt.Println("\n e1元素移除后 Remove ")
 	for e := l.Front(); e != nil; e = e.Next() {
 		fmt.Print(e.Value," ")
 	}
+	// init 可以用作 clear
 	l.Init()
-	fmt.Println("\n list init 后 ")
+	fmt.Println("\n list init()后 ")
 	for e := l.Front(); e != nil; e = e.Next() {
 		fmt.Print(e.Value," ")
 	}
-	fmt.Println("list 的长度",l.Len())
+	fmt.Println("list 的长度Init  ",l.Len())
 	//向前移动
 	//
 
